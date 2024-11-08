7235a31c88ffa1915ddf9b5d10caa1ae7d20bfa5
noeldnhe <henri.noeldner@frg.wwschool.de>
2024-11-08 10:35:30 +0100

    Implementirung des Klassendiagrammes bis setGt. Noch nicht befuelt.

---
+++ src/Ceasar.java
@@ -0,0 +1,35 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Ceasar {
+    private String gt;
+    private String Kt;
+    private int schluessel;
+
+    public Ceasar(){
+
+    }
+    public void verschluesseln(){
+
+    }
+    public void entschluesseln(){
+
+    }
+    private char getChar(int pWert){
+        return 'i';
+    }
+    private int getAscif(char pWert){
+        return 1;
+    }
+
+    public String getGt(){
+        return gt;
+    }
+
+    public void setGt(String pGt){
+
+    }
+
+
+
+
+}
