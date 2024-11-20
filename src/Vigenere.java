49b7c81af067329a014af54312ff615f35574520
noeldnhe <henri.noeldner@frg.wwschool.de>
2024-11-20 11:15:25 +0100

    Kommentierung in Vignere

--- src/Vigenere.java
+++ src/Vigenere.java
@@ -11,24 +11,43 @@ public class Vigenere {
         s="a";
     }
     public void verschluesseln(){
+        //geheimtext wird gelehrt
         gt="";
+        //tkt wirde auf eine großgeschriebene Variante von kt gesetzt
         String tkt= kt.toUpperCase();
-        System.out.println(kt);
-        String TemporerString= s.toUpperCase();
+        //gSchluessel wirde auf eine großgeschriebene Variante von kt gesetzt
+        String gSchluessel= s.toUpperCase();
         for(int i=0;i<tkt.length();i++){
+            //Wandelt den Buchstaben an der Stelle i in eine Zahl um und speichert diese in h
             int h = this.getAscif(tkt.charAt(i));
-            int stemporer=( TemporerString.charAt(i%TemporerString.length())-65)%26;
-            gt= gt + this.getChar(((h+stemporer-39)%26)+65) ;
+            //sTemporer wird auf den asciff wert vom Buchstaben an Stelle gestzt
+            //-65 damirt A=0 und %26 damit der wert in einer spaeteren Rechnung funktioniert (damit nicht groeßer als 26)
+            int stemporer=( gSchluessel.charAt(i%gSchluessel.length())-65)%26;
+            //Addirt h und den schlüssel und sorgt dafuer, das das ergebnis ein Großbuchstbe ist
+            //-13, da wir teoretisch auch durch -65 rechnen um den asciff von A gleich 0 zu setzten. Da wir im nachste Schritt modula zu 26 rechnen reicht -13 aus(65%26=13)
+            //%26, um zu verhindern, dass unsere Zahl aus dem Alphabet hinausgeht.
+            //+65 damit unsere Zahl wieder den Großbuchstaben i Asciff entspricht
+            gt= gt + this.getChar(((h+stemporer-13)%26)+65) ;
         }
     }
     public void entschluesseln(){
+        //Klartext wird gelehrt
         kt="";
+        //tkt wirde auf eine großgeschriebene Variante von kt gesetzt
         String tgt= gt.toUpperCase();
-        String TemporerString= s.toUpperCase();
+        //gSchluessel wirde auf eine großgeschriebene Variante von kt gesetzt
+        String gSchluessel= s.toUpperCase();
         for(int i=0;i<tgt.length();i++) {
+            //Wandelt den Buchstaben an der Stelle i in eine Zahl um und speichert diese in h
             int h = this.getAscif(tgt.charAt(i));
-            int stemporer=(TemporerString.charAt(i%TemporerString.length())-65)%26;
-            kt +=  this.getChar(((h-stemporer-39)%26)+65);
+            //sTemporer wird auf den asciff wert vom Buchstaben an Stelle gestzt
+            //-65 damirt A=0 und %26 damit der wert in einer spaeteren Rechnung funktioniert (damit nicht groeßer als 26)
+            int stemporer=(gSchluessel.charAt(i%gSchluessel.length())-65)%26;
+            //Addirt h und stemporer und sorgt dafuer, das das ergebnis ein Großbuchstbe ist
+            //-13, da wir teoretisch auch durch -65 rechnen um den asciff von A gleich 0 zu setzten. Um zu verhindern, das unsere Zahl negativ wirde rechnen wir nur -13 (kleinste zahl 65 - größte zahl 26 -13>0
+            //%26, um zu verhindern, dass unsere Zahl aus dem Alphabet hinausgeht.
+            //+65 damit unsere Zahl wieder den Großbuchstaben i Asciff entspricht
+            kt +=  this.getChar(((h-stemporer-13)%26)+65);
         }
     }
 
