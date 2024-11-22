9f3c16a2569e46ded4df85786fcf194e17250a88
Henrinoeldner <henri.noeldner@frg.wwschool.de>
2024-11-22 23:16:17 +0100

    Rechtschreibung korrigiert

--- src/Ceasar.java
+++ src/Ceasar.java
@@ -3,7 +3,7 @@
 public class Ceasar extends Kryptomat{
     private int s;
 
-    //imkonstuktor werden gt, kt und s auf null beziunghweise auf ein lehren String gesetzt
+    //im constructor werden gt, kt und s auf null beziunghweise auf ein lehren String gesetzt
     public Ceasar(){
         gt="";
         kt="";
@@ -14,14 +14,14 @@ public class Ceasar extends Kryptomat{
         String speicher;
         //geheimtext wird gelehrt
         gt="";
-        //tkt wirde auf eine großgeschriebene wariante von kt gestzt
+        //tkt wird auf eine großgeschriebene variante von kt gesetzt
         String tkt= kt.toUpperCase();
         for(int i=0;i<tkt.length();i++){
             //Wandelt den Buchstaben an der Stelle i in eine Zahl um und speichert diese in h
             int h = this.getAscif(tkt.charAt(i));
-            //Addirt h und den schlüssel und sorgt dafuer, das das ergebnis ein Großbuchstbe ist
-            //s%26, da für uns zahlen die groeßer sid als 26 nicht relewant sind, sondern nur der rest. Verhindert das sie Zahl, mit der wir weiter rechnen zu groß wird
-            //-13, da wir teoretisch auch durch -65 rechnen um den asciff von A gleich 0 zu setzten. Da wir im nachste Schritt modula zu 26 rechnen reicht -13 aus(65%26=13)
+            //Addirt h und den schlüssel und sorgt dafuer, das das ergebnis ein Großbuchstabe ist
+            //s%26, da für uns zahlen die groeßer sid als 26 nicht relevant sind, sondern nur der rest. Verhindert das sie Zahl, mit der wir weiter rechnen zu groß wird
+            //-13, da wir teoretisch auch durch -65 rechnen um den asciff von A gleich 0 zu setzten. Da wir im naechste Schritt modula zu 26 rechnen reicht -13 aus(65%26=13)
             //%26, um zu verhindern, dass unsere Zahl aus dem Alphabet hinausgeht.
             //+65 damit unsere Zahl wieder den Großbuchstaben i Asciff entspricht
             speicher= String.valueOf(this.getChar(((h+(s%26)-13)%26) +65));
@@ -38,14 +38,14 @@ public class Ceasar extends Kryptomat{
         String speicher;
         //der Klartext(kt) wird gelehrt
         kt="";
-        //tgt wird auf Eine Großgeschreibene variante von gt gestzt
+        //tgt wird auf Eine großgeschriebene variante von gt gesetzt
         String tgt= gt.toUpperCase();
         for(int i=0;i<tgt.length();i++) {
             // h wird auf den asciff wert von dem Buchstaben von tgt an stelle i gesetzt
             int h = this.getAscif(tgt.charAt(i));
-            //Addirt h und den schlüssel und sorgt dafuer, das das ergebnis ein Großbuchstbe ist
+            //Addirt h und den schlüssel und sorgt dafuer, das das ergebnis ein Großbuchstabe ist
             //s%26, da für uns zahlen die groeßer sid als 26 nicht relewant sind, sondern nur der rest. Verhindert das sie Zahl, mit der wir weiter rechnen zu groß wird
-            //-13, da wir teoretisch auch durch -65 rechnen um den asciff von A gleich 0 zu setzten. Um zu verhindern das unsere Zahl negativ wirde rechnen wir nur -13 (kleinste zahl 65 - größte zahl 26 -13>0
+            //-13, da wir teoretisch auch durch -65 rechnen um den asciff von A gleich 0 zu setzten. Um zu verhindern, dass unsere Zahl negativ wirde rechnen wir nur -13 (kleinste zahl 65 - größte zahl 26 -13>0
             //%26, um zu verhindern, dass unsere Zahl aus dem Alphabet hinausgeht.
             //+65 damit unsere Zahl wieder den Großbuchstaben i Asciff entspricht
             speicher= String.valueOf(this.getChar(((h-(s%26)-13)%26) +65));
--- src/Vigenere.java
+++ src/Vigenere.java
@@ -13,18 +13,18 @@ public class Vigenere extends Kryptomat  {
         String speicher;
         //geheimtext wird gelehrt
         gt="";
-        //tkt wirde auf eine großgeschriebene Variante von kt gesetzt
+        //tkt wird auf eine großgeschriebene Variante von kt gesetzt
         String tkt= kt.toUpperCase();
         //gSchluessel wirde auf eine großgeschriebene Variante von kt gesetzt
         String gSchluessel= s.toUpperCase();
         for(int i=0;i<tkt.length();i++){
             //Wandelt den Buchstaben an der Stelle i in eine Zahl um und speichert diese in h
             int h = this.getAscif(tkt.charAt(i));
-            //sTemporer wird auf den asciff wert vom Buchstaben an Stelle gestzt
-            //-65 damirt A=0 und %26 damit der wert in einer spaeteren Rechnung funktioniert (damit nicht groeßer als 26)
+            //sTemporer wird auf den asciff wert vom Buchstaben an Stelle gesetzt
+            //-65 damit A=0 und %26 damit der wert in einer spaeteren Rechnung funktioniert (damit nicht groeßer als 26)
             int stemporer=( gSchluessel.charAt(i%gSchluessel.length())-65)%26;
-            //Addirt h und den schlüssel und sorgt dafuer, das das ergebnis ein Großbuchstbe ist
-            //-13, da wir teoretisch auch durch -65 rechnen um den asciff von A gleich 0 zu setzten. Da wir im nachste Schritt modula zu 26 rechnen reicht -13 aus(65%26=13)
+            //Addirt h und den schlüssel und sorgt dafuer, das das ergebnis ein Großbuchstabe ist
+            //-13, da wir teoretisch auch durch -65 rechnen um den asciff von A gleich 0 zu setzten. Da wir im naechste Schritt modula zu 26 rechnen reicht -13 aus(65%26=13)
             //%26, um zu verhindern, dass unsere Zahl aus dem Alphabet hinausgeht.
             //+65 damit unsere Zahl wieder den Großbuchstaben i Asciff entspricht
             speicher= String.valueOf(this.getChar(((h+stemporer-13)%26)+65));
@@ -40,17 +40,17 @@ public class Vigenere extends Kryptomat  {
         String speicher;
         //Klartext wird gelehrt
         kt="";
-        //tkt wirde auf eine großgeschriebene Variante von kt gesetzt
+        //tkt wird auf eine großgeschriebene Variante von kt gesetzt
         String tgt= gt.toUpperCase();
-        //gSchluessel wirde auf eine großgeschriebene Variante von kt gesetzt
+        //gSchluessel wird auf eine großgeschriebene Variante von kt gesetzt
         String gSchluessel= s.toUpperCase();
         for(int i=0;i<tgt.length();i++) {
             //Wandelt den Buchstaben an der Stelle i in eine Zahl um und speichert diese in h
             int h = this.getAscif(tgt.charAt(i));
-            //sTemporer wird auf den asciff wert vom Buchstaben an Stelle gestzt
-            //-65 damirt A=0 und %26 damit der wert in einer spaeteren Rechnung funktioniert (damit nicht groeßer als 26)
+            //sTemporer wird auf den asciff wert vom Buchstaben an Stelle gesetzt
+            //-65 damit A=0 und %26 damit der wert in einer spaeteren Rechnung funktioniert (damit nicht groeßer als 26)
             int stemporer=(gSchluessel.charAt(i%gSchluessel.length())-65)%26;
-            //Addirt h und stemporer und sorgt dafuer, das das ergebnis ein Großbuchstbe ist
+            //Addirt h und stemporer und sorgt dafuer, das das ergebnis ein Großbuchstabe ist
             //-13, da wir teoretisch auch durch -65 rechnen um den asciff von A gleich 0 zu setzten. Um zu verhindern, das unsere Zahl negativ wirde rechnen wir nur -13 (kleinste zahl 65 - größte zahl 26 -13>0
             //%26, um zu verhindern, dass unsere Zahl aus dem Alphabet hinausgeht.
             //+65 damit unsere Zahl wieder den Großbuchstaben i Asciff entspricht
