78db72982e77597d5ef942e63653b19358956c94
noeldnhe <henri.noeldner@frg.wwschool.de>
2024-11-26 13:26:12 +0100

    POlybius angefangen

---
+++ src/Polybius.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Polybius extends Kryptomat{
+    private int[][] alphabetQuadrat;
+    public Polybius(){
+        gt="";
+        kt="";
+        s="0";
+    }
+    //verschluesseln() verschlüsselt den Klartext(kt) durch addition mit dem schlüssel(s)
+    public void verschluesseln(){
+
+    }
+    public void entschluesseln(){
+
+    }
+}
--- src/verwaltung.java
+++ src/verwaltung.java
@@ -8,14 +8,14 @@ public class verwaltung {
     public static void main(String[] args) {
 
         new verwaltung();
-        System.out.println("Herzlich willkommen zuZeichenketten verschluesselerer!");
+        System.out.println("Herzlich willkommen zu Zeichenketten verschluesselerer!");
 
     }
     public verwaltung(){
         scanner = new Scanner(System.in);
         while (true) {
             System.out.println("== HAUPTMENÜ ==");
-            System.out.println("Bitte weahle mit welcher methode du versclüsseln moechtst");
+            System.out.println("Bitte weahle mit welcher methode du verschluesseln moechtest");
             System.out.println("[1]Ceasar");
             System.out.println("[2]Vigenere");
             System.out.println("[3]Beenden");
@@ -27,23 +27,23 @@ public class verwaltung {
             }else if (verschluesselungprinzip>2){
                 break;
             }
-                System.out.println("Was moechtst du machen? Gib die Enstprechende Zahl an und drueck Enter. ");
-                System.out.println("[1] Ein Wort neues Wort verschlüsseln (Benötigt ein KlarText und ein Schluessel)");
-                System.out.println("[2] Ein wort neues Wort entschlüsseln(benoetigt ein GeheimText und ein Schlussel)");
+                System.out.println("Was moechtst du machen? Gib die enstsprechende Zahl an und drueck Enter. ");
+                System.out.println("[1] Ein Wort neues Wort verschluesseln (Benoetigt ein KlarText und ein Schluessel)");
+                System.out.println("[2] Ein wort neues Wort entschluesseln(benoetigt ein GeheimText und ein Schlussel)");
 
                 int option = scanner.nextInt();
                 if (option == 1) {
-                    System.out.println("Bitte gib den zu verschlüsselden text an. Druecke dann Enter");
+                    System.out.println("Bitte gib den zu verschluesselden text an. Druecke dann Enter");
                     String optionString = scanner.next();
                     verschluesseler.setKt(optionString);
-                    System.out.print("Bitte gib den schlüssel an");
+                    System.out.print("Bitte gib den schluessel an");
                     if (verschluesselungprinzip==1){System.out.print("(Eine Zahl oder ein Buchstaben)");}
                     else if (verschluesselungprinzip==2) {System.out.print("(Ein Wort)");}
                     System.out.println("Druecke dann Enter.");
                     optionString = scanner.next();
                     verschluesseler.setS(optionString);
                     verschluesseler.verschluesseln();
-                    System.out.println("Dein verschluesselte wort ist: " + verschluesseler.getGt());
+                    System.out.println("Dein verschluesseltes Wort ist: " + verschluesseler.getGt());
                 }
                 if (option == 2) {
                     System.out.println("Bitte gib den verschlüsselten text an. ");
